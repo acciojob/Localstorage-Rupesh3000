7caf0b694ba60c0c6c4087db74b875fbe08f8cd8
Rupesh3000 <101381357+Rupesh3000@users.noreply.github.com>
2026-02-10 22:39:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
-{}
+const addItems = document.querySelector(".add-items");
+const itemsList = document.querySelector(".plates");
+const items = JSON.parse(localStorage.getItem("items")) || [];
+
+function populateList(items = [], itemsList) {
+  if (!items.length) return;
+  itemsList.innerHTML = items
+    .map((plate, i) => {
+      return `
+        <li>
+          <input 
+            type="checkbox" 
+            data-index="${i}" 
+            id="item${i}" 
+            ${plate.done ? "checked" : ""}
+          />
+          <label for="item${i}">${plate.text}</label>
+        </li>
+      `;
+    })
+    .join("");
+}
+
+// 🔥 render on page load
+populateList(items, itemsList);
+
+const handalSubmit = (event) => {
+  event.preventDefault();
+
+  const form = event.target;
+  const formData = new FormData(form);
+  const itemValue = formData.get("item").trim();
+
+  if (!itemValue) return;
+
+  items.push({
+    text: itemValue,
+    done: false,
+  });
+
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
+  form.reset();
+};
+
+function toggleDone(e) {
+  if (!e.target.matches('input[type="checkbox"]')) return;
+
+  const index = e.target.dataset.index;
+  items[index].done = !items[index].done;
+
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+addItems.addEventListener("submit", handalSubmit);
+itemsList.addEventListener("click", toggleDone);
